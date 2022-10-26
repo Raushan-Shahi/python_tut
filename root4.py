5f4345cde2fccf6237405fb8c332f5338f33b77b
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-27 01:53:35 +0530

    string addition side by side

---
+++ root4.py
@@ -0,0 +1,6 @@
+# Practice of root3.py
+
+name = input('Write your name- ')
+colour = input('What is your favourite colour? ')
+food = input('What is your favourite food? ')
+print(name + ' likes ' + colour + 'colour and likes to eat ' + food )
