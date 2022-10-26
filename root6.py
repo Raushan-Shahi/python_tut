b69756e7c91fc20e7a4b9a9f835f35620eff96ee
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-27 02:20:02 +0530

    string

---
+++ root6.py
@@ -0,0 +1,12 @@
+#string 
+#accessing data wi the help of string data 
+
+data = '''My name is raushan'''
+print(data)
+print(data[:])
+print(data[1:])
+print(data[5:])
+print(data[:7])
+print(data[:8])
+print(data[:9])
+print(data[2:6])
