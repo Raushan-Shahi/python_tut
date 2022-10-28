d65d81ee4d66ace97fd4d0460424e4608e72b5e2
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-28 20:14:27 +0530

    string functions like len, upper, lower, find, replace, etc

---
+++ root8.py
@@ -0,0 +1,12 @@
+# Different funcions related to string 
+# len() function is not restricted to only strings, it can be used anywhere
+
+
+details = 'I am a second year student at MNIT'
+print(len(details))
+print(details.upper())
+print(details)
+print(details.find('e'))
+print(details.find('I am'))
+print(details.replace('second year', 'sophomore'))
+print('second' in details)          #retunrs a boolean value
