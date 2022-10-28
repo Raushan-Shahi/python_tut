fe13761658a7b46cab65eb4e80b3fab0ecce4099
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-28 21:42:51 +0530

    math functions

---
+++ root11.py
@@ -0,0 +1,11 @@
+#maths functions
+# read math madule to know all the functions of math library
+# import math is used to use math library
+
+import math
+x = 2.9
+print(round(x))
+print(abs(-2.9))
+math.ceil(2.9)
+math.floor(2.4
+)
