49bf41d340568c51b3619d352b414dda4f268c0b
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-28 21:50:04 +0530

    if statement

---
+++ root12.py
@@ -0,0 +1,25 @@
+#if statement 
+# syntax
+# if x:
+# elif y:
+# else:
+# x and y are defined earlier in the form of boolean 
+
+
+
+
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+
+else:
+    print("It's a pleasant weather")
+
+print("Enjoy your day")
