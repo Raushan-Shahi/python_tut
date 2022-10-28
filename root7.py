8f29b19f64d1b5c48505402ac97fa8220e0231a3
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-28 16:14:45 +0530

    formatted string

---
+++ root7.py
@@ -0,0 +1,10 @@
+#formatted string 
+#used to increase visibility and reduce compexity
+
+
+first = 'Raushan'
+last = 'Shahi'
+message = first + ' [' + last + '] is a coder'
+msg = f'{first} [{last}] is a coder'
+print(message)
+print(msg)
