01e019717f757e639bc028bd03dabd268a06e024
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-28 20:38:17 +0530

    arithematic operators

---
+++ root9.py
@@ -0,0 +1,16 @@
+#arithmatic operations
+#augumented operator
+
+print(10+3)
+print(10-3)
+print(10 * 3)
+print(10/3)
+print(10 % 3)
+print(10 // 3)          # give an integer putput instead of float
+print(10 ** 3)          # used to get exponential values
+
+x = 10
+x = x+3
+print(x)
+x += 3              #augumented operator
+print(x)
