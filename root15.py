491c992e87fb1b29f280d3fc0ef2072782ecf486
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-31 18:42:20 +0530

    if statement practice

---
+++ root15.py
@@ -0,0 +1,14 @@
+#Comparison Operators
+# if temp is more than 30 it is hot 
+#if temp is less than 10 it is cold
+#else it is good day
+
+
+temperature = 23
+
+if temperature >30:
+    print("It is a hot day")
+elif temperature <10:
+    print("It is a cold day")
+else:
+    print("It is a pleasant day")
