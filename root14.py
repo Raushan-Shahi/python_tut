64c9c666615fddc1ee1852dcc60999084af436b6
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-31 18:37:33 +0530

    logical operators

---
+++ root14.py
@@ -0,0 +1,12 @@
+#logical operators
+#if applicant has high income AND good credit the n eligible for loan 
+# and-> both true 
+# or - > atleast one true
+# not -> negation 
+
+
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
