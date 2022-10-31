d1872ee130fd950dacbd26b765c427b5824587a1
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-31 18:52:10 +0530

    if statement

---
+++ root16.py
@@ -0,0 +1,14 @@
+#if statement 
+#length of string comparison 
+
+
+
+name = input("enter your name: ")
+length = len(name)
+
+if length < 3:
+    print("Name must be at least 3 characters")
+elif length > 50:
+    print("name can be maximum 50 characcters")
+else:
+    print("the name looks good")
