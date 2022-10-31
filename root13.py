b0b957180e69c28e3c91f2820aa0981fee8f66fc
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-10-31 18:32:34 +0530

    example for if statement

---
+++ root13.py
@@ -0,0 +1,16 @@
+#example for if-else statement 
+#if it has good credit then the down payment is 10%credit 
+# else it is 20% credit
+
+
+price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+
+else:
+    down_payment = 0.2 * price
+
+print(f"Down payment:{down_payment}")
+
