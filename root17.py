2acb048079de6a996048c368768566e1b332eec4
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-11-01 22:05:23 +0530

    weight converter project

---
+++ root17.py
@@ -0,0 +1,25 @@
+# project weight converter
+
+
+weight = int(input("Enter your weight: " ))
+unit = input("enter your unit:(l)lbs or (k)kgor (g)grams" ) 
+convert = input("The unit you want " )
+
+if unit == "l" and convert == "k":
+    print(f'{weight*4.5} {convet}')
+
+elif unit == "k" and convert == "l":
+    print(f'{weight/4.5} {convert}')
+elif unit == "k" and convert == "g":
+    print(f'{weight*1000} {convert}')
+elif unit =="g" and convert == "k":
+    print(f'{weight/1000} {convert}')
+elif unit == "g" and convert == "l":
+    print(f'{weight/4500} {convert}')
+else:
+    print(f'{weight*4500} {convert}')
+
+
+
+
+
