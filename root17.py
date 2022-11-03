02d1bbbcea16b17cca718feaa03cb70781d3041c
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-11-03 23:53:01 +0530

    weight converter

--- root17.py
+++ root17.py
@@ -1,6 +1,6 @@
 # project weight converter
 
-print("Welcone to weight calculator")
+print("weight converter")
 weight = int(input("Enter your weight: " ))
 unit = input("enter your unit:(l)lbs or (k)kgor (g)grams" ) 
 convert = input("The unit you want " )
