31c6ae3838667963209b350ec4e2a1533194c079
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-11-05 23:53:46 +0530

    weight converter

--- root17.py
+++ root17.py
@@ -1,6 +1,6 @@
 # project weight converter
 
-print("weight converter")
+print("weight converter (kg,g,lbs)")
 weight = int(input("Enter your weight: " ))
 unit = input("enter your unit:(l)lbs or (k)kgor (g)grams" ) 
 convert = input("The unit you want " )
