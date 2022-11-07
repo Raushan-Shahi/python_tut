f985e3899d695a453d77568b32ec73255841e26b
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-11-07 23:42:27 +0530

    wwight calculator

--- root17.py
+++ root17.py
@@ -19,7 +19,7 @@ elif unit == "g" and convert == "l":
 else:
     print(f'{weight*4500} {convert}')
 
-print("thank you for using the calculator.")
+print("thank you for using the  weight convertor.")
 
 
 
