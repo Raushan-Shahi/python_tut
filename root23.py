d32af3150f767217d0d275a45fbaf0616480459d
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-11-19 22:37:14 +0530

    for loop examples

---
+++ root23.py
@@ -0,0 +1,14 @@
+for x in range(26):
+    print(x)
+
+for x in [25]:
+    print(x)
+
+    def greet_friends(friends):
+        for friend in friends:
+            print("hi " + friend)
+
+greet_friends(['Taylor' , 'Lusia', 'Jamaal' , 'Eli'])
+
+greet_friends("Barrry")
+greet_friends(["Barry"])
