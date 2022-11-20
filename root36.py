63aca25ad74620dffe5e7df7c9f0243bc97397df
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-11-21 00:29:42 +0530

    list operations

---
+++ root36.py
@@ -0,0 +1,21 @@
+# list operations 
+
+fruits = ["pineapple", "mango", "apple"," banana"]
+fruits.append("kiwi")
+
+print(fruits)
+
+fruits.insert(0,'orange')
+print(fruits)
+
+fruits.insert(66, "melon")
+print(fruits)
+
+fruits.remove("orange")
+print(fruits)
+
+fruits.pop(4)
+print(fruits)
+
+fruits[2] = "strawberry"
+print(fruits)
