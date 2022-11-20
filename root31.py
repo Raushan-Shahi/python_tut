830a9a4f253a5d081df69bd5d5ae1a7dc8fd7162
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-11-20 16:09:23 +0530

    string operations(inbuilt functions)

---
+++ root31.py
@@ -0,0 +1,21 @@
+#string operations 
+
+word = "   I have interest in the FIELD of Artificial Intelligence   "
+initial_1= "Local Area network"
+initial_2 = "Mahendra singh dhoni"
+
+print(word.upper())
+print()
+print(word.lower())
+print()
+print(word.strip())
+print()
+print(word.lstrip())
+print()
+print(word.rstrip())
+print()
+print(word.split())
+print(" ".join(["Raushan", "is", "a", "student", "of", "civil"]))
+print()
+print(". . ->".join(["Raushan", "is", "a", "student", "of", "civil"]))
+
