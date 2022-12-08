69375d71ddd883ea74d5943d3b239247eee4c10d
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-12-09 01:10:51 +0530

    pracitice

--- root51.py
+++ root51.py
@@ -1,12 +1,25 @@
-#Use a list comprehension to create a list of squared numbers (n*n).
-#The function receives the variables start and end, 
-# and returns a list of squares of consecutive numbers between start and end inclusively.
-#For example, squares(2, 3) should return [4, 9].
+#Question 3
+#A professor with two assistants, Jamie and Drew, wants an attendance list of the students, 
+# in the order that they arrived in the classroom. 
+# Drew was the first one to note which students arrived, and then Jamie took over. 
+# After the class, they each entered their lists into the computer and emailed them to the professor, 
+# who needs to combine them into one, in the order of each student's arrival. Jamie emailed a follow-up, 
+# saying that her list is in reverse order. 
+# Complete the steps to combine them into one list as follows: the contents of Drew's list, 
+# followed by Jamie's list in reverse order, to get an accurate list of the students as they arrived.
 
 
-def squares(start, end):
-	return [ ___ ]
 
-print(squares(2, 3)) # Should be [4, 9]
-print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
-print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+def combine_lists(list1, list2):
+  list1.reverse()
+  list2.extend(list1)
+  return list2
+  # Generate a new list containing the elements of list2
+  # Followed by the elements of list1 in reverse order
+  
+	
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+
+print(combine_lists(Jamies_list, Drews_list))
