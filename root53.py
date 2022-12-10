3381eecb78deb72f08dea6e1e4674fc6de8d2d48
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-12-10 15:31:25 +0530

    oops, classes

---
+++ root53.py
@@ -0,0 +1,19 @@
+#Object oriented programming in python
+#Definition of classes
+#keep the first letter of a class in uppercase  
+
+
+class Flower:
+  color = ''
+
+rose = Flower()
+rose.color = "red"
+
+violet = Flower()
+violet.color = "blue"
+
+this_pun_is_for_you = "this chair is for you"
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(this_pun_is_for_you) 
