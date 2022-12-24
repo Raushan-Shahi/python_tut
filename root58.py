da24af92bb3be3e88620e7a2e01a78b0e1f09786
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-12-24 13:39:57 +0530

    sort function

---
+++ root58.py
@@ -0,0 +1,10 @@
+#sorting in python
+
+numbers = [1,6,4,3,67,23,12]
+numbers.sort()
+print(numbers)
+
+names = ["Raushan", "Shubhansu", "vivek", "Rishabh", "Saurabh"]
+print(sorted(names))
+#sorted function creates a new list while the sort function changes the current list
+print(names)
